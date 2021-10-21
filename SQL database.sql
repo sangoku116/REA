158ef01be8f9c02e9e15cec93b874c332f8b42ba
Gudepengu <71423720+eeparreno@users.noreply.github.com>
2021-10-21 00:45:00 -0400

    Add files via upload

---
+++ SQL database.sql
@@ -0,0 +1,17 @@
+use Tanpa_Nama;
+
+create table Admins (
+  UserID INT,
+  Username VARCHAR (255),
+  Passwords VARCHAR(100)
+  LoginTime DATETIME 
+);
+
+create table Reports (
+    Submission_Date DATE,
+    Submission_Time TIME,
+    Report_Title varchar (100),
+    ReportID int,
+    Report_Description varchar (255), 
+    
+)
