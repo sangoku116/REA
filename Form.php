bfc926ec6227e16d9f511bbe01b083b54a712e38
Hello <52844014+clareesespieces@users.noreply.github.com>
2021-11-11 11:54:00 -0500

    Includes; Image upload
    Also contains ReportID and success message

--- Form.php
+++ Form.php
@@ -9,7 +9,6 @@
 
 <?php
 
-
 // define variables and set to empty values
 $titleErr = $commentErr = $dateErr = "";
 $title = $comment = $date = "";
@@ -32,6 +31,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["date"])) {
         $dateErr = "Date is required";
     }
+
+    if($_FILES["anyfile"]["error"] != 0){
+        $fileErr =  "No file uploaded";
+    }
+
 }
 
 function test_input($data) {
@@ -44,7 +48,7 @@ function test_input($data) {
 
 <h2>Anonymous Reporting Form</h2>
 <p><span class="error">* required field</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" enctype="multipart/form-data">
     Title: <input type="text" name="title" value="<?php echo $title;?>">
     <span class="error">* <?php echo $titleErr;?></span>
     <br><br>
@@ -54,11 +58,12 @@ function test_input($data) {
     Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
     <span class="error">* <?php echo $commentErr;?></span>
     <br><br>
+    <label for="file_name"> Files:</label>
+    <input type="file" name="anyfile" id="anyfile">
+    <br><br>
     <input type="submit" name="submit" value="Submit">
     <a href="Admin_login.php" class="btn btn-info" role="button">Admin mode</a>
 </form>
-
-
 <?php
 
 include "database_connect.php";
@@ -67,19 +72,17 @@ function f_alert($message) {
     echo "<script>alert('$message');</script>";
 }
 
-
 if (!empty($titleErr) OR (!empty($dateErr)) OR (!empty($commentErr))){
     f_alert('The submission was not successful. Please check for errors and try again.');
 } elseif (!empty($title) OR (!empty($comment)) OR (!empty($date))) {
-    $_POST=array();
+    // $_POST=array();
     //$sql = "INSERT INTO Reports (ReportID, Report_title, Submission_Date, Report_Description) VALUES (?, ?, ?, ?)";
     $sql = "INSERT INTO Reports (Report_title, Submission_Date, Report_Description) VALUES (?, ?, ?)";
 
     if($result = mysqli_prepare($link, $sql)){
         //Bind param to sql statement
-         //mysqli_stmt_bind_param($result, "isss", $t,$ReportTitle, $ReportDate, $ReportComment);
+        //mysqli_stmt_bind_param($result, "isss", $t,$ReportTitle, $ReportDate, $ReportComment);
         mysqli_stmt_bind_param($result,"sss", $ReportTitle, $ReportDate, $ReportComment);
-        //$t = time();
         $ReportTitle = $title;
         $ReportDate = $date;
         $ReportComment = $comment;
@@ -89,6 +92,44 @@ if (!empty($titleErr) OR (!empty($dateErr)) OR (!empty($commentErr))){
             $t = time();
             $sqlID = "UPDATE Reports SET ReportID = '$t' WHERE Report_title = '$title' AND Submission_Date = '$date' AND Report_Description = '$comment'";
             $ReportID = mysqli_query($link, $sqlID);
+
+            // if there is a file submitted with the Rest of the reports
+            if($_FILES["anyfile"]["error"] == 0){
+
+                //Check if file was uploaded with no error
+                if(isset($_FILES["anyfile"]) && $_FILES["anyfile"]["error"] == 0){
+                    $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png", "heif" => "image/heif");
+                    $filename = $_FILES["anyfile"]["name"];
+                    $filetype = $_FILES["anyfile"]["type"];
+                    $filesize = $_FILES["anyfile"]["size"];
+
+                    //Validate file extension
+                    $ext = pathinfo($filename, PATHINFO_EXTENSION);
+                    if(!array_key_exists($ext, $allowed)) die("Error: Please select a valid file format.");
+
+                    //Validate File size - Max 10MB
+                    $maxsize = 10*1024*1024;
+                    if($filesize > $maxsize) die("Error: File size is larger than 10MB");
+
+                    //Validate File type
+                    if(in_array($filetype, $allowed)){
+
+                        //Folder where file will be stored
+                        $upload_folder = "upload/";
+
+                        //Check whether file exists before uploading it
+                        if(move_uploaded_file($_FILES["anyfile"]["tmp_name"], $upload_folder.$filename)){
+                            $stmt = "UPDATE Reports SET File = '$filename' WHERE ReportID = '$t'";
+                            mysqli_query($link, $stmt);
+                            echo "File uploaded!";
+                        } else{
+                            echo "File not uploaded.";
+                        }
+                    } else{
+                        echo "Error: There was a problem uploading your file";
+                    }
+                }
+            }
             echo '<script>alert("Submission Success! Your ReportID is '.$t.'")</script>';
             ///f_alert('Submission Success!');
         }
@@ -96,15 +137,9 @@ if (!empty($titleErr) OR (!empty($dateErr)) OR (!empty($commentErr))){
 
     }
 }
+
 mysqli_close($link);
-
-// -------------- ANOTHER ELSE STATEMENT FOR WHEN CONNECTION CANNOT BE ESTABLISHED -------------------------
-//else {
-//    f_alert('Continue');
-
-//}
 ?>
-
 </body>
 </html>
 
