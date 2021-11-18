65cf10afa7245b5d19f4b53a07e56e3e0b214d31
Hello <52844014+clareesespieces@users.noreply.github.com>
2021-11-18 13:58:39 -0500

    Includes: CSS and table for submissions

--- welcome.php
+++ welcome.php
@@ -7,38 +7,48 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
    header("location: Admin_login.php");
     exit;
 }
+
+include_once "database_connect.php";
+
+$sql = 'SELECT * FROM Reports';
+$result = mysqli_query($link,$sql);
+
+// selects all file info from db and sets them to array called $files
+$files = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body{ font: 14px sans-serif; text-align: center; }
-    </style>
+    <title>Test</title>
+    <link rel="stylesheet" href="style1.css">
 </head>
 <body>
-<h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+<h1 class="center-title">Hello, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
 <p>
-    <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+    <a href="logout.php" class="button">Sign Out of Your Account</a>
 </p>
-<?php
-include_once 'database_connect.php';
-$display = "SELECT * FROM Reports";
-$result = mysqli_query($link, $display);
-while ($row = mysqli_fetch_array($result)){
-    echo "<img src='upload/".$row['File']."'>";
-    echo "<br><br>";
-    echo "<p>".$row["Report_title"]."</p>";
-    echo "<br><br>";
-    echo "<p>".$row["Submission_Date"]."</p>";
-    echo "<br><br>";
-    echo "<p>".$row["Report_Description"]."</p>";
-    echo "<br>";
-}
-?>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Filename</th>
+        <th>Event Date</th>
+        <th>Report Description</th>
+        <th>Action</th>
+    </tr>
+    <tbody>
+    <?php foreach ($files as $file): ?>
+        <tr>
+            <td><?php echo $file['ReportID']; ?></td>
+            <td><?php echo $file['Report_title']; ?></td>
+            <td><?php echo $file['Submission_Date']; ?></td>
+            <td><?php echo $file['Report_Description']; ?></td>
+            <td><a href="upload/<?php echo $file['File']?>">View</a></td>
+        </tr>
+    <?php endforeach;?>
+    </tbody>
+</table>
 </body>
 </html>
 
