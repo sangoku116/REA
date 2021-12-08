40c77428262b7316406a8d5ed797ff490f1e5f3d
Hello <52844014+clareesespieces@users.noreply.github.com>
2021-12-08 18:04:17 -0500

    allows welcome page to check for report time

---
+++ view.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once "database_connect.php";
+if (isset($_GET['file_id'])) {
+    $id = $_GET['file_id'];
+
+    // fetch file to download from database
+    $sql = "SELECT * FROM Reports WHERE ReportID='$id'";
+    $result = mysqli_query($link, $sql);
+
+    $file = mysqli_fetch_assoc($result);
+    $filepath = 'upload/'.$file['File'];
+
+    header("location: http://localhost:63342/REA/".$filepath);
+
+    // Now update downloads count
+    $check = date('Y-m-d H:i:s');
+    $updateQuery = "UPDATE Reports SET checked='$check' WHERE ReportID='$id'";
+    mysqli_query($link, $updateQuery);
+    exit;
+
+
+}
+?>
